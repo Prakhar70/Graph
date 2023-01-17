29548bec6c85c48b67e967c28faecc5a1327c6fd
Prakhar70 <prakharagarwal70@gmail.com>
2023-01-17 22:17:50 +0530

    level lec1

---
+++ LevelUP/Question.java
@@ -0,0 +1,191 @@
+package LevelUP;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    
+    /*
+     * 200
+     * 695
+     * 463
+     * 130
+     */
+    //lc 200
+    // https://leetcode.com/problems/number-of-islands/description/
+    public int numIslands(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int cnt=0;
+        int[][] dir= {{0,1},{1,0},{0,-1},{-1,0}};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    dfs(grid,i,j,dir);
+                }
+            }
+        }
+        return cnt;
+        
+    }
+    public static void dfs(char[][] grid,int sr,int sc,int[][] dir){
+       int n=grid.length;
+       int m=grid[0].length;
+        grid[sr][sc]='0';
+        for(int[] d:dir){
+            int r=sr+d[0];
+            int c=sc+d[1];
+            if(r>=0 && r<n && c>=0 && c<m && grid[r][c]!='0'){
+                dfs(grid,r,c,dir);
+            }
+        }
+    }
+    //lc 695
+    // https://leetcode.com/problems/max-area-of-island/
+    public int maxAreaOfIsland(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int ans=0;
+        int[][] dir={{0,1},{1,0},{0,-1},{-1,0}};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    ans=Math.max(ans,maxAreaOfComp(grid,i,j,dir));
+                }
+            }
+        }
+        return ans;
+    }
+    public int maxAreaOfComp(int[][] grid,int sr, int sc,int[][] dir){
+        int m=grid.length;
+        int n=grid[0].length;
+        grid[sr][sc]=0;
+        int area=0;
+        for(int[] d:dir){
+            int r=sr+d[0];
+            int c=sc+d[1];
+            if(r>=0 && r<m && c>=0 && c<n && grid[r][c]==1){
+                area+=maxAreaOfComp(grid,r,c,dir);
+            }
+        }
+        return area+1;
+
+    }
+    // lc 463 
+    //https://leetcode.com/problems/island-perimeter/
+    public int islandPerimeter(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    ans+=4;
+                    if(i-1>=0 && grid[i-1][j]==1){
+                        ans--;
+                    }
+                    if(i+1<n && grid[i+1][j]==1){
+                        ans--;
+                    }
+                    if(j-1>=0 && grid[i][j-1]==1){
+                        ans--;
+                    }
+                    if(j+1 <m && grid[i][j+1]==1){
+                        ans--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    //leetcode 130
+    // https://leetcode.com/problems/surrounded-regions/
+    public void solve(char[][] board) {
+        int[][] dir={{0,1},{1,0},{-1,0},{0,-1}};
+        int n=board.length;
+        int m=board[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0||i==n-1||j==m-1){
+                    dfs_(board,i,j,dir);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='$')
+                    board[i][j]='O';
+                else 
+                    board[i][j]='X';
+            }
+        }
+        
+    }
+    public void dfs_(char[][] board, int sr,int sc,int[][] dir){
+        int n=board.length;
+        int m=board[0].length;
+        board[sr][sc]='$';
+        for(int[] d:dir){
+            int r=sr+d[0];
+            int c=sc+d[1];
+            if(r>=0 && r<n && c>=0 && c<m && board[r][c]=='O'){
+                dfs_(board,r,c,dir);
+            }
+        }
+    }
+
+    // https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+
+    public static long journeyToMoon(int n, List<List<Integer>> astronaut) {
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (List<Integer> list : astronaut) {
+            System.out.println(list);
+            int u = list.get(0);
+            int v = list.get(1);
+
+            graph[u].add(v);
+            graph[v].add(u);
+
+        }
+
+        ArrayList<Long> num_comp_list = new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == false) {
+                long noc = dfs(graph, vis, i);
+                num_comp_list.add(noc);
+            }
+        }
+
+        long sum = 0;
+        long ans = 0;
+        for (int i = num_comp_list.size() - 1; i >= 0; i--) {
+            long curr_ele = num_comp_list.get(i);
+            ans = ans + (sum * curr_ele);
+            sum += curr_ele;
+
+        }
+        return ans;
+    }
+
+    static long dfs(ArrayList<Integer> graph[], boolean[] vis, int src) {
+        long cnt = 0;
+        vis[src] = true;
+        for (int i : graph[src]) {
+            if (!vis[i]) {
+                cnt += dfs(graph, vis, i);
+            }
+        }
+        return cnt + 1;
+    }
+
+
+    /************ */
+
+}
--- LevelUP/l001.java
+++ LevelUP/l001.java
@@ -1,5 +1,135 @@
 package LevelUP;
 
+import java.util.ArrayList;
+
+//Level 2 class 1 =========DFS===============
 public class l001 {
+
+    public static class Edge {
+        int v = 0, w = 0;
+
+        Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w) {
+        graph[u].add(new Edge(v, w));
+        graph[v].add(new Edge(u, w));
+    }
+
+    public static void display(ArrayList<Edge>[] graph) {
+
+        int N = graph.length;
+        for (int i = 0; i < N; i++) {
+
+            System.out.print(i + "-->");
+            for (Edge e : graph[i]) {
+
+                System.out.println("(" + e.v + ", " + e.w + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int findEdge(ArrayList<Edge>[] graph, int u, int v) {
+
+        for (int i = 0; i < graph[u].size(); i++) {
+            if (graph[u].get(i).v == v) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void removeEdge(ArrayList<Edge>[] graph, int u, int v) {
+        int idx1 = findEdge(graph, u, v);
+        graph[u].remove(idx1);
+
+        int idx2 = findEdge(graph, v, u);
+        graph[v].remove(idx2);
+    }
+
+    // hasPath
+    // O(E), where E is total no of edges in that particular component.
+    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) {
+        if (src == dest) {
+            return true;
+        }
+        vis[src] = true;
+        boolean res = false;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                res = res || hasPath(graph, e.v, dest, vis);
+            }
+        }
+        return res;
+    }
+
+    // Find Path
+    public static int printAllPath(ArrayList<Edge>[] graph, int src, int dest, String psf, int wsf, boolean[] vis) {
+        if (src == dest) {
+            System.out.println(psf + dest + "@" + wsf);
+            return 1;
+        }
+        vis[src] = true;
+        int cnt = 0;
+        for (Edge e : graph[src]) {
+            if (!vis[e.v]) {
+                cnt += printAllPath(graph, e.v, dest, psf + src, wsf + e.w, vis);
+            }
+        }
+        vis[src] = false;
+        return cnt;
+    }
+
+    // Get connected component
+    //O(E+V)
+    public static int getConnectedComponent(ArrayList<Edge>[] graph){
+        int N=graph.length;
+        int comp=0;
+        boolean[] vis=new boolean[N];
+        for(int i=0;i<N;i++){
+            if(!vis[i]){
+                comp++;
+                dfs_GCC(graph,i,vis);
+            }
+        }
+        return comp;
+    }
+
+    public static void dfs_GCC(ArrayList<Edge>[] graph,int src,boolean[] vis){
+        vis[src]=true;
+        for(Edge e:graph[src]){
+            if(!vis[e.v])
+                dfs_GCC(graph,e.v,vis);
+        }
+    }
+    public static void constructGraph() {
+        int N = 7;
+        ArrayList<Edge>[] graph = new ArrayList[N];
+        for (int i = 0; i < N; i++)
+            graph[i] = new ArrayList<>();
+
+        addEdge(graph, 0, 1, 10);
+        addEdge(graph, 1, 2, 10);
+        addEdge(graph, 2, 3, 40);
+        addEdge(graph, 3, 0, 10);
+        addEdge(graph, 3, 4, 2);
+        addEdge(graph, 4, 5, 2);
+        addEdge(graph, 5, 6, 3);
+        addEdge(graph, 6, 4, 8);
+
+        display(graph);
+        boolean[] vis = new boolean[N];
+        // System.out.println(dfs_findPath(graph, 0, 6, vis));
+        System.out.println(printAllPath(graph, 0, 6, "", 0, vis));
+    }
+
+    public static void main(String[] args) {
+        constructGraph();
+    }
     
+
 }
